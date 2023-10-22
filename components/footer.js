49bef5a9f2501e67e6c90f77f09ccc9346a425d9
Nguyen Giap <ngoctuan.cen@gmail.com>
2023-10-22 11:34:22 +0700

    deloy test

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <a>Footer</a>
+    )
+}
---
+++ components/nested-layout.js
@@ -0,0 +1,5 @@
+export default function NestedLayout() {
+    return (
+      <a>NestedLayout</a>
+    )
+  }
